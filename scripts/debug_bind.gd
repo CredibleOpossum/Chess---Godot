de4b5d38997b776f65b898406fcaca9075be710d
CredibleOpossum <CredibleOpossum@protonmail.com>
2023-06-28 13:01:07 -0400

    fixes

--- scripts/debug_bind.gd
+++ scripts/debug_bind.gd
@@ -1,4 +1,5 @@
 extends Timer
 
+# this is a dirty hack, I will implement a Rust scene switcher to solve this
 func _on_timeout():
-	self.get_tree().change_scene_to_file("res://scenes/debug_scene.tscn")
+	self.get_tree().change_scene_to_file("res://scenes/main_chess_scene.tscn")
