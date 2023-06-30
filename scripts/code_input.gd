f35137b75bec586bf3cd6d6c68b16a196f63b581
CredibleOpossum <CredibleOpossum@protonmail.com>
2023-06-30 02:39:03 -0400

    update visuals

--- scripts/code_input.gd
+++ scripts/code_input.gd
@@ -1,5 +1,7 @@
 extends Node
 
 func _on_text_submitted(new_text):
-	self.get_node("/root/WaitLogic/WaitRoom").try_connect(new_text)
+	var input_num = int(new_text)
+	if input_num > 0 && input_num != GenerateAndWait.number:
+		self.get_node("/root/WaitLogic/WaitRoom").try_connect(new_text)
  
